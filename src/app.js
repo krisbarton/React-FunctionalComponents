f5546670bf34ac4c0883deea54575ef5a284ca44
Kris Barton <krisbarton@gmail.com>
2020-09-24 12:15:46 +0100

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FunctionalComponent from './components/functionalComponent';
+import ClassComponent from './components/classComponent';
+
+const App = () => {
+    return (
+        <>
+            <FunctionalComponent />
+            <ClassComponent />
+        </>
+    )
+}
+
+export default App;
